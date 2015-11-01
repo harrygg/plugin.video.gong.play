37083b4b10654ff6f00d662c84144e50f224932f
Hristo Genev <harrygg@gmail.com>
2015-11-01 16:26:53 +0200

    Added Play API

--- plugin.video.gong.play/addon.py
+++ plugin.video.gong.play/addon.py
@@ -1,12 +1,30 @@
 ﻿# -*- coding: utf-8 -*-
-import re, sys, os.path, urllib, urllib2, cookielib, urlparse
+import re, sys, os.path, urllib, urllib2, cookielib, urlparse, base64
 import xbmc, xbmcgui, xbmcplugin, xbmcaddon
 from resources.lib.gongplay import GongPlay
 
+# append pydev remote debugger
+REMOTE_DBG = True
+if REMOTE_DBG:
+	try:
+		sys.path.append("C:\\Software\\Java\\eclipse-luna\\plugins\\org.python.pydev_4.4.0.201510052309\\pysrc")
+		import pydevd
+		xbmc.log("After import pydevd")
+		#import pysrc.pydevd as pydevd # with the addon script.module.pydevd, only use `import pydevd`
+		# stdoutToServer and stderrToServer redirect stdout and stderr to eclipse console
+		pydevd.settrace('localhost', stdoutToServer=False, stderrToServer=False, suspend=False)
+	except ImportError:
+		xbmc.log("Error: You must add org.python.pydev.debug.pysrc to your PYTHONPATH.")
+		sys.exit(1)
+	except:
+		xbmc.log("Unexpected error:", sys.exc_info()[0]) 
+		sys.exit(1)
+		
 reload(sys)  
 sys.setdefaultencoding('utf8')
 _addon = xbmcaddon.Addon(id='plugin.video.gong.play')
 _language = _addon.getLocalizedString
+isApiEngine = True if _addon.getSetting('engine') == "0" else False 
 gong = GongPlay(_addon)
 
 def CATEGORIES():	
@@ -20,38 +38,56 @@ def CATEGORIES():
 		msg = _language(30204) + gong.expiration_date
 		xbmc.executebuiltin('Notification(%s,%s,15000,%s)'%(gong.display_name, msg, gong.icon))
 
-def CLIPS(url):
+
+def GAMES(url = ''):
+	addDir(_language(30209).encode('utf8'), gong.url_video_clips, 4)
+	if isApiEngine:
+		loggedin = gong.login()
+		if loggedin:
+			if gong.is_payment_expired:
+				xbmcgui.Dialog().ok(gong.addon_name, _language(30203) + gong.expiration_date)
+		else:
+			xbmcgui.Dialog().ok(gong.addon_name, _language(30205))
+			
+	games = gong.get_games(url)
+	for i in range(0, len(games)):
+		text = games[i]['text'] % _language(30210) if '%s' in games[i]['text'] else games[i]['text'] 
+		addDir(text.encode('utf8'), games[i]['url'], 2, games[i]['icon'])
+		
+def STREAMS(match_url):
+	xbmc.log("match_url: " + match_url)
+	name = urllib.unquote_plus(params["name"])
+	if isApiEngine:
+		streams = gong.get_game_stream(match_url)
+		addLink(name + " 1", streams[0], 3)
+		addLink(name + " 2", streams[0], 3)
+	else:
+		streams = gong.get_game_stream(match_url)
+		if len(streams) == 0:
+			if gong.is_loggedin == False:
+				xbmcgui.Dialog().ok(gong.addon_name, _language(30205))
+			else:
+				if gong.is_payment_expired:
+					xbmcgui.Dialog().ok(gong.addon_name, _language(30203) + gong.expiration_date)
+				else:
+					xbmc.executebuiltin('Notification(%s,%s,5000,%s)'%(gong.addon_name, _language(30206), gong.icon))
+		else:
+			addLink(name + "  SD", streams[0], 3)
+			if (len(streams)) > 1:
+				addLink( name + "  HD", streams[1], 3)
+
+def VIDEOCLIPS(url):
 	video_clips = gong.get_video_clips(url)
 	for i in range(0, len(video_clips)):
 		addLink(video_clips[i]['text'], video_clips[i]['id'], 5, video_clips[i]['icon'])
 	#Generate the link url for the next 20 items
 	start = str(int(vbstart) + 20)
-	next_page_url = 'http://m.vbox7.com/user/clips.do?usr=gongbg&ajax=1&start=%s&order=date' % start
+	next_page_url = base64.b64decode('aHR0cDovL20udmJveDcuY29tL3VzZXIvY2xpcHMuZG8/dXNyPWdvbmdiZyZhamF4PTEmc3RhcnQ9JXMmb3JkZXI9ZGF0ZQ==') % start
 	addDir(_language(30211).encode('utf8'), next_page_url, 4, start)
 
 def PLAYVBOXSTREAM(id):
 	PLAY(gong.get_clip_stream(id))
 
-def GAMES(url):
-	games = gong.get_games(url)
-	for i in range(0, len(games)):
-		text = games[i]['text'] % _language(30210) if '%s' in games[i]['text'] else games[i]['text'] 
-		addDir(text.encode('utf8'), games[i]['url'], 2)
-		
-def STREAMS(match_url):
-	streams = gong.get_game_stream(match_url)
-	if len(streams) == 0:
-		if gong.is_loggedin == False:
-			xbmcgui.Dialog().ok(gong.addon_name, _language(30205))
-		else:
-			if gong.is_payment_expired == True:
-				xbmcgui.Dialog().ok(gong.addon_name, _language(30203) + gong.expiration_date)
-			else:
-				xbmc.executebuiltin('Notification(%s,%s,5000,%s)'%(gong.addon_name, _language(30206), gong.icon))
-	else:
-		addLink(gong.game_title + " SD", streams[0], 3)
-		if (len(streams)) > 1:
-			addLink(gong.game_title + " HD", streams[1], 3)
 
 def PLAY(url):
 	li = xbmcgui.ListItem(iconImage=gong.icon, thumbnailImage=gong.icon, path=url)
@@ -61,10 +97,10 @@ def PLAY(url):
 def addLink(name, url, mode, icon = ''):
 	return addItem(name, url, mode, False, icon)
 
-def addDir(name, url, mode, vbstart = ''):
-	return addItem(name, url, mode, True, '', vbstart)
+def addDir(name, url, mode, icon = '', vbstart = ''):
+	return addItem(name, url, mode, True, icon, vbstart)
 
-def addItem(name, url, mode, isDir, icon = '', vbstart = ''):
+def addItem(name, url, mode, isDir, icon, vbstart = ''):
 	item_icon = icon if icon != '' else gong.icon
 	u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&vbstart="+vbstart
 	ok=True
@@ -119,10 +155,13 @@ try:
 except:
 	pass
 	
-if mode==None or url==None or len(url)<1:
+if isApiEngine == False and (mode==None or url==None or len(url) < 1):
 	CATEGORIES()
-    
-elif mode==1:
+
+elif isApiEngine and (mode==None or url==None or len(url) < 1):
+	GAMES()
+	
+elif mode == 1:
 	GAMES(url)
 
 elif mode==2:
@@ -132,7 +171,7 @@ elif mode==3:
 	PLAY(url)
 
 elif mode==4:
-	CLIPS(url)
+	VIDEOCLIPS(url)
 	
 elif mode==5:
 	PLAYVBOXSTREAM(url)
--- plugin.video.gong.play/resources/lib/gongplay.py
+++ plugin.video.gong.play/resources/lib/gongplay.py
@@ -1,4 +1,5 @@
-﻿import xbmc, xbmcaddon, urllib, urllib2, cookielib, urlparse, os.path, sys, re
+﻿import xbmc, xbmcaddon, urllib, urllib2, cookielib, urlparse, os.path, sys, re, hashlib, time, json, gzip, base64
+from StringIO import StringIO
 from redirecthandler import GPHTTPRedirectHandler
 
 reload(sys)  
@@ -15,6 +16,7 @@ class GongPlay:
 	subscription_msg = ''
 	game_title = ''
 	addon_name = ''
+	addon_id = ''
 	username = ''
 	password = ''
 	debug = False
@@ -23,22 +25,37 @@ class GongPlay:
 	cookie_file_vbox = ''
 	icon = ''
 	#urls
-	url_main = 'http://play.gong.bg/'
-	url_fixtures = 'http://play.gong.bg/fixture'
-	url_video_clips = 'http://vbox7.com/user:gongbg?p=allvideos'
-	url_vbox_resolver = 'http://vbox7.com/etc/ext.do?key='
+	url_main = base64.b64decode('aHR0cDovL3BsYXkuZ29uZy5iZy8=')
+	url_login = base64.b64decode('aHR0cDovL3BsYXkuZ29uZy5iZy9zaWduaW4=')
+	url_fixtures = base64.b64decode('aHR0cDovL3BsYXkuZ29uZy5iZy9maXh0dXJl')
+	url_playapi = base64.b64decode('aHR0cDovL3BsYXlhcGkuZ29uZy5iZy9hcGkvZml4dHVyZXM/aGFzaD0=')
+	url_playlogin = base64.b64decode('aHR0cDovL3BsYXlhcGkuZ29uZy5iZy9hcGkvc2lnbmluP2hhc2g9')
+	url_video_clips = base64.b64decode('aHR0cDovL3Zib3g3LmNvbS91c2VyOmdvbmdiZz9wPWFsbHZpZGVvcw==')
+	url_vbox_resolver = base64.b64decode('aHR0cDovL3Zib3g3LmNvbS9ldGMvZXh0LmRvP2tleT0=')
+	api_hash = ''
+	#session_id = ''
 	#User agents
+	user_agent = ''
 	ua_mobile = 'Mozilla/5.0 (Linux; Android 5.0.2; bg-bg; SAMSUNG GT-I9195 Build/JDQ39) AppleWebKit/535.19 (KHTML, like Gecko) Version/1.0 Chrome/18.0.1025.308 Mobile Safari/535.19' 
 	ua_pc = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
 	last_response = ''
-
+	isApiEngine = True
+	
 	def __init__(self, addon):
 		self.addon_name = addon.getAddonInfo('name')
+		self.addon_id = addon.getAddonInfo('id')
 		self.icon = xbmc.translatePath(os.path.join(addon.getAddonInfo('path'), "icon.png"))
 		self.username = addon.getSetting('username')
 		self.password = addon.getSetting('password')
+		self.isApiEngine = True if addon.getSetting('engine') == "0" else False 
 		self.debug = True if addon.getSetting('debug') == 'true' else False
 		xbmc.log("plugin.video.gong.play | debugging log is set to " + str(self.debug))
+		self.api_hash = self.hash1()
+		self.user_agent = self.ua_mobile
+		if self.isApiEngine:
+			self.url_playapi = self.url_playapi + self.api_hash
+			self.url_login = self.url_playlogin + self.api_hash
+			self.user_agent = 'okhttp/2.3.0'
 		profile = xbmc.translatePath( addon.getAddonInfo('profile'))
 		self.cookie_file = os.path.join(profile, '.cookies')
 		self.cookie_file_vbox = os.path.join(profile, '.vboxcookie')
@@ -46,42 +63,66 @@ class GongPlay:
 		opener = urllib2.build_opener(GPHTTPRedirectHandler, cookieprocessor)
 		urllib2.install_opener(opener)
 
-	def request(self, url, ua = ua_mobile, rf = url_main):
+	def request(self, url, ua = user_agent, rf = url_main):
 		if os.path.isfile(self.cookie_file):
 			self.cj.load(self.cookie_file)
 		req = urllib2.Request(url)
 		req.add_header('User-Agent', ua)
 		req.add_header('Referer', rf)
+		req.add_header('Accept', '*/*')
+		req.add_header('Accept-Encoding', 'gzip')
 		res = urllib2.urlopen(req)
-		self.last_response = res.read()
-		if self.debug: xbmc.log("plugin.video.gong.play | Received response for URL:" + url + "\r\n" + self.last_response)
+		self.last_response = self.parse_gzip(res)
+		#if self.debug: xbmc.log("plugin.video.gong.play | Received response for URL:" + url + "\r\n" + self.last_response)
 		res.close()
-		if not 'vbox7' in url:
-			self.set_user_info()
-
+		if not 'vbox7' in url and self.isApiEngine == False:
+			self.isLoggedIn()
+	
+	def parse_gzip(self, res):
+		if res.info().get('Content-Encoding') == 'gzip':
+		    buf = StringIO(res.read())
+		    f = gzip.GzipFile(fileobj = buf)
+		    return f.read()
+		else:
+			return res.read()
+			
 	def login(self):
 		post_data = urllib.urlencode({'email' : self.username, 'password' : self.password})
 		xbmc.log(post_data)
-		req = urllib2.Request('http://play.gong.bg/signin', post_data)
-		req.add_header('User-Agent', self.ua_mobile)
-		req.add_header('Content-Type', "application/x-www-form-urlencoded")
+		req = urllib2.Request(self.url_login, post_data)
+		req.add_header('User-Agent', self.user_agent)
+		req.add_header('Content-Type', "application/x-www-form-urlencoded; charset=UTF-8")
+		req.add_header('Accept-Encoding', 'gzip')
 		res = urllib2.urlopen(req)
-		self.last_response = res.read()
+		self.last_response = self.parse_gzip(res)
 		res.close()
 		self.cj.save(self.cookie_file, ignore_discard=True)
-		self.set_user_info()
-	
-	def set_user_info(self):
-		#Limit the scope of search by getting the user info dir
-		user_div = re.compile('user-info\">(.*?)</div', re.DOTALL).findall(self.last_response)
-		if (len(user_div)) > 0:
-			self.get_loggedin(user_div[0])	
-			self.get_display_name(user_div[0])
-			self.get_payment_info(user_div[0])
-			
-	def get_loggedin(self, text):
-		matches = re.compile('(/signout\")').findall(text)
-		self.is_loggedin = True if len(matches) > 0 else False
+		return self.isLoggedIn()
+		
+	def isLoggedIn(self):
+
+		if self.isApiEngine:
+			json_response = json.loads(self.last_response)
+			reg_status = json_response['reg_status'][0]
+			#validto element exists only when login is successful
+			try:
+				self.expiration_date =  reg_status['validto'] #'2015-11-17 18:43:09'
+				validTo = re.sub('[^\d]', '', self.expiration_date)
+				now = time.strftime("%Y%m%d%H%M00")
+				self.is_payment_expired = True if validTo <= now else False
+				#self.session_id = reg_status['session_id']
+				self.is_loggedin = True
+			except AttributeError:
+				self.is_loggedin =  False
+		else:
+			user_div = re.compile('user-info["\'\s]>(.*?)</div', re.DOTALL).findall(self.last_response)
+			if (len(user_div)) > 0:
+				matches = re.compile('(/signout["\'\s]{1})').findall(user_div[0])
+				self.is_loggedin = True if len(matches) > 0 else False
+				self.get_display_name(user_div[0])
+				self.get_payment_info(user_div[0])
+				self.is_loggedin = True
+		return self.is_loggedin
 
 	def get_display_name(self, text):
 		matches = re.compile('user-name.*>(.*?)</a').findall(text)
@@ -99,15 +140,16 @@ class GongPlay:
 		self.is_payment_expired = True if len(expired) > 0 else False
 		
 	def get_categories(self):
+		categories = []
 		self.request(self.url_fixtures)
 		matches = self.find_regex('ul.+program-nav.+tablist[\s"\']+>(.*?)</ul', re.DOTALL)
-		categories = []
+		
 		if len(matches) != 0:
 			#Find out category links and titles. 
 			hrefs = re.compile('href[\s="\']*(.*?)["\'\s]+aria').findall(matches[0])
-			if self.debug: xbmc.log("plugin.video.gong.play | get_categories | Found " + str(len(hrefs)) + " matches for regex 'href[\s=\"\']*(.*?)[\"\'\s]+aria'")
+			if self.debug: xbmc.log(self.addon_id + " | get_categories | Found " + str(len(hrefs)) + " matches for regex 'href[\s=\"\']*(.*?)[\"\'\s]+aria'")
 			names = re.compile('p.+?>(.*?)</p').findall(matches[0])
-			if self.debug: xbmc.log("plugin.video.gong.play | get_categories | Found " + str(len(names)) + " matches for regex 'p.+?>(.*?)</p'")
+			if self.debug: xbmc.log(self.addon_id + " | get_categories | Found " + str(len(names)) + " matches for regex 'p.+?>(.*?)</p'")
 			# The hrefs are always one more than the category names
 			if len(names) == len(hrefs) - 1:
 				categories.append({'text' : 'Програма - Всички категории', 'url' : hrefs[0]})
@@ -118,59 +160,107 @@ class GongPlay:
 					category['text'] = title.replace('<br>', '')
 					category['url'] =  urlparse.urljoin(self.url_main, hrefs[i+1])
 					categories.append(category)
-					
+
 		return categories
 
 	def get_games(self, url):
 		games = []
-		
-		self.request(urlparse.urljoin(self.url_main, url))
-		dates = self.find_regex('date-info[\s\"]*.*>(.*?)</')
-		hours = self.find_regex('time-info[\s\"]*.*>(.*?)</')
-		details = self.find_regex('href[\s=]*\"(.*)\".*title[\s=]*\"(.*)\".*class[\s=\"]*.*btn-table.*(regular|live).*\".*>')
-		if len(dates) == len(hours) and len(hours) == len(details):
-			for i in range(0, len(dates)):
-				title = "| [COLOR white]" + details[i][1] + "[/COLOR]"
-				if details[i][2] == "live":
-					live = "[COLOR green][B]%s[/B][/COLOR]" 
-					title = "[COLOR white]" + live + " " + details[i][1] + "[/COLOR]"
-				game = {}
-				game['url'] = urlparse.urljoin(self.url_main, urllib.quote(details[i][0]))
-				game['text'] = '[CAPITALIZE][B]' + dates[i][:9] + " " + hours[i] + "[/B][/CAPITALIZE] " + title
-				games.append(game)
-				
-		if self.debug : xbmc.log("plugin.video.gong.play | get_games | Found " + str(len(games)) + " games on " + url)
-		return games
+		self.request(self.url_playapi)
 
+		if self.isApiEngine:
+			json_response = json.loads(self.last_response)
+			epg = json_response['epg']
+			for i in range(0, len(epg)):
+				teams = epg[i]["home_name"] + " - " + epg[i]["away_name"]
+				title  = "| [COLOR white]" + teams + "[/COLOR]"
+				if self.is_game_started(epg[i]['showstart']):
+					live = "[COLOR green][B]%s[/B][/COLOR]"
+					title = "[COLOR white]" + live + " " + teams + "[/COLOR]"	
+				game = {}
+				game['url'] = epg[i]["show_id"]
+				game['text'] = "[B]" + epg[i]["showstart"][:16] + "[/B] " + title
+				game['icon'] = epg[i]['portrait']
+				games.append(game)
+		else:
+			self.request(self.url_fixtures)
+			dates = self.find_regex('date-info[\s\'"]+.*>(.*?)</')
+			hours = self.find_regex('time-info[\s\'"]+.*>(.*?)</')
+			details = self.find_regex('href[\s="\']+(.*)"+.*title[\s=\'"]+(.*)"+.*class[=\s"\']+.*btn-table.*(regular|live)')
+			if len(dates) == len(hours) and len(hours) == len(details):
+				for i in range(0, len(dates)):
+					title = "| [COLOR white]" + details[i][1] + "[/COLOR]"
+					if details[i][2].lower() == "live":
+						live = "[COLOR green][B]%s[/B][/COLOR]" 
+						title = "[COLOR white]" + live + " " + details[i][1] + "[/COLOR]"
+					game = {}
+					game['url'] = urlparse.urljoin(self.url_main, urllib.quote(details[i][0]))
+					game['text'] = '[B]' + dates[i][:9] + " " + hours[i] + "[/B] " + title
+					game['icon'] = ''
+					games.append(game)
+					
+			if self.debug : xbmc.log(self.addon_id + " | get_games | Found " + str(len(games)) + " games on " + url)
+		return games
+	
+	
+	def is_game_started(self, datetime):
+		# strip anything that's not a digital "2015-11-02 17:30:00"
+		showstart = re.sub('[^\d]', '', datetime)
+		now = time.strftime("%Y%m%d%H%M00")
+		return True if showstart <= now else False
+		
 	def get_game_stream(self, url_game):
-		if self.is_loggedin == False:
-			self.login()
-		self.request(url_game)
 		#streams = ["http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8"]
 		streams = []
-		matches = self.find_regex('title>(.*?)<')
-		if len(matches) > 0:
-			self.game_title = matches[0]
-		matches = self.find_regex('iframe.+src[="\'\s]+(.*cdn.*?)[\'"\s]+')
-		if len(matches) > 0:
-			if self.debug : xbmc.log("plugin.video.gong.play | Found Iframe url=" + matches[0])
-			url_iframe = matches[0]
-			self.request(matches[0])
-			video = self.find_regex('video.+src[="\']+(.*?)[\'"\s]+')
-			if len(video) > 0:
-				xbmc.log("plugin.video.gong.play | Found video url=" + video[0])
-				streams.append(video[0])
-				streams.append(re.sub('_(1)\.s', "_2.s", video[0]))
+		
+		if self.isApiEngine:
+			stream = self.get_mobile_streams(url_game, 'stream_android')
+			streams.append(stream)
+			stream = self.get_mobile_streams(url_game, 'stream_iphone')
+			streams.append(stream)
+		else:
+			if self.is_loggedin == False:
+				self.login()
+			self.request(url_game)
+
+			matches = self.find_regex('iframe.+src[="\'\s]+(.*cdn.*?)[\'"\s]+')
+			if len(matches) > 0:
+				if self.debug : xbmc.log(self.addon_id + " | Found Iframe url=" + matches[0])
+				url_iframe = matches[0]
+				self.request(matches[0])
+				video = self.find_regex('video.+src[="\']+(.*?)[\'"\s]+')
+				if len(video) > 0:
+					xbmc.log(self.addon_id + " | get_game_stream | Found video url=" + video[0])
+					streams.append(video[0])
+					streams.append(re.sub('_(1)\.s', "_2.s", video[0]))
 				
 		return streams
-
+	
+	def get_mobile_streams(self, show_id, type):
+		try:
+			session_id = ''
+			with open(self.cookie_file, 'r') as c: data = c.read()
+			match = re.compile('PHPSESSID=(.*?);').findall(data)
+			if len(match) > 0:
+				session_id = match[0]
+				
+			self.request(base64.b64decode("aHR0cDovL3BsYXlhcGkuZ29uZy5iZy9hcGkvbWF0Y2gv") + show_id + '/' + session_id + '?hash=' + self.api_hash)
+			json_response = json.loads(self.last_response)
+			reg_status = json_response['reg_status'][0]
+			stream = reg_status[type] + self.hash2()
+			xbmc.log(self.addon_id + " | get_mobile_streams(show_id="+show_id+", type="+type+") | Found stream url=" + stream)
+		except Exception:
+			stream = ''
+			
+		return stream
+		
+		
 	def find_regex(self, exp, flags=re.IGNORECASE):
 		matches = re.compile(exp, flags).findall(self.last_response)
-		if self.debug: xbmc.log("plugin.video.gong.play | find_regex | Found " + str(len(matches)) + " matches for regex '" + exp + "', flags=" + str(flags))
+		if self.debug: xbmc.log(self.addon_id + " | find_regex | Found " + str(len(matches)) + " matches for regex '" + exp + "', flags=" + str(flags))
 		return matches
 		
 	def get_video_clips(self, url):
-		self.request(url)
+		self.request(url, self.ua_mobile, '')
 		video_clips = []
 		matches = self.find_regex('a.*href=\"/play:([0-9a-zA-Z]{10})\".*img.*src=\"(.*?)\".*alt=\"(.*?)\"')
 		if len(matches) > 0:
@@ -185,6 +275,26 @@ class GongPlay:
 	
 	def get_clip_stream(self, id):
 		self.request(self.url_vbox_resolver + id, self.ua_pc, self.url_video_clips)
-		matches = self.find_regex('flv_addr=(.*?)&')	
-		
+		matches = self.find_regex('flv_addr=(.*?)&')
 		return matches[0] if len(matches) > 0 else ''
+	
+	
+	def hash1(self):
+		m = hashlib.md5()
+		pwd = base64.b64decode("JTAjNEwzNjlwY2JNSHRsYzlFblpQR25XdSNNUzZIMiM=")
+		m.update(pwd + time.strftime("%Y%m%d"))
+		return m.hexdigest()
+	
+	def hash2(self):
+		m = hashlib.md5()
+		pwd = base64.b64decode("JHJzZHcuLnRmZWV3M0AzMzNsOzQt")
+		m.update(pwd + self.get_ip() + '-' + time.strftime("%d.%m.%Y"))
+		return m.hexdigest()
+	
+	def get_ip(self):
+		url = base64.b64decode('aHR0cDovL2dvbmcuc21hcnR4bWVkaWEuZXUvaXAucGhw')
+		self.request(url)
+		json_response = json.loads(self.last_response)
+		geo_ip = json_response['geo_ip'][0]
+		ip = geo_ip['ip']
+		return ip
